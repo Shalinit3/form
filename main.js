0dab37d3f9e05b7717694811f759b023f6091b6d
Shalinit3 <shalinityagi64@gmail.com>
2017-12-06 19:04:29 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+function printdata(){
+var fname=document.getElementById("fname").value;
+var lname=document.getElementById("lname").value;
+var gender=document.getElementsByName("gender").value;
+var email=document.getElementById("email").value;
+var phone=document.getElementById("phone").value;
+var state=document.getElementById("state").value;
+var country=document.getElementById("nation").value;
+var dob=document.getElementById("dob").value;
+
+console.log("Name: "+fname);
+}
